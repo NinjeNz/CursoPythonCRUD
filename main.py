49feee583aa9ea6f1b476eb258b6ed795470e5d2
NinjeNz <111711390+NinjeNz@users.noreply.github.com>
2023-07-05 17:52:07 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,28 @@
 
 clientes = 'pablo,ricardo,'
 
-if __name__ == '__main__':
-    clientes += 'David'
+
+def crear_cliente(nombre_cliente):
+    global clientes
+   
+    clientes += nombre_cliente
+    _add_coma()
+
+
+def lista_clientes():
+    global clientes
+
     print(clientes)
+
+def _add_coma():
+    global clientes
+    
+    clientes += ','
+
+
+if __name__ == '__main__':
+    lista_clientes()
+
+    crear_cliente('Luis')
+   
+    lista_clientes()

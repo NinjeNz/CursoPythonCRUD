d9eff59391bf0dee456f96b353c3dd650b0c25d6
NinjeNz <111711390+NinjeNz@users.noreply.github.com>
2023-08-08 17:29:03 -0700

    uso de diccionarioss

--- main.py
+++ main.py
@@ -36,40 +36,6 @@ clientes = [
 ]
 
 
-def crear_cliente(client):  # def crear_cliente(nombre_cliente):
-    global clientes
-    if client not in clientes:
-        clientes.append(client)  # clientes += nombre_cliente
-        # _add_coma() -- Ya no se necesita desde que la variable clientes paso de ser string a una lista.
-    else:
-        print('EL cliente ya se encuentra en la lista de clientes')
-
-
-def actualizar_cliente(nombre_cliente, nombre_cliente_actualizado):
-    global clientes
-    if nombre_cliente in clientes:
-        index = clientes.index(nombre_cliente)
-        clientes[index] = nombre_cliente_actualizado
-    else:
-        print('Cliente no se encuentra en la lista de clientes')
-
-
-def borrar_cliente(nombre_cliente):
-    global clientes
-    if nombre_cliente in clientes:
-        clientes.remove(nombre_cliente)
-    else:
-        print('El cliente no se encuentra registrado en la lista de clientes')
-
-
-def buscar_cliente(nombre_cliente):
-    for cliente in clientes:
-        if cliente != nombre_cliente:
-            continue
-        else:
-            return True
-
-
 def lista_clientes():
     """if clientes:
         print('Lista de Clientes :')
@@ -90,26 +56,73 @@ def lista_clientes():
         )
 
 
-def _print_welcome():
-    # print('BIENVENIDO A' + FRED + ' RUGGERI VENTAS' + FR)
-    # print('*' * 50)
-    # print('Que quieres hacer hoy?')
-    print(FYE + '[C]rear Cliente' + FR)
-    print(FYE + '[L]ista Clientes' + FR)
-    print(FYE + '[E]liminar Cliente' + FR)
-    print(FYE + '[A]ctualizara Cliente' + FR)
-    print(FYE + '[B]uscar' + FR)
-    print(FYE + '[S]alir' + FR)
+def crear_cliente(cliente):  # def crear_cliente(nombre_cliente):
+    global clientes
+    if cliente not in clientes:
+        clientes.append(cliente)  # clientes += nombre_cliente
+        # _add_coma() -- Ya no se necesita desde que la variable clientes paso de ser string a una lista.
+    else:
+        print('EL cliente ya se encuentra en la lista de clientes')
 
 
-def _get_client_field(field_name):
+def actualizar_cliente(cliente_id, cliente_actualizado):
+    global clientes
+    if len(clientes) - 1 >= cliente_id:
+        clientes[cliente_id] = cliente_actualizado
+        # index = clientes.index(nombre_cliente)
+        # clientes[index] = nombre_cliente_actualizado
+    else:
+        print('Cliente no se encuentra en la lista de clientes')
+
+
+def borrar_cliente(cliente_id):
+    global clientes
+
+    for idx, cliente in enumerate(clientes):
+        if idx == cliente_id:
+            del clientes[idx]
+            break
+    # if nombre_cliente in clientes:
+        # clientes.remove(nombre_cliente)
+        else:
+            print('El cliente no se encuentra registrado en la lista de clientes')
+
+
+def buscar_cliente(nombre_cliente):
+    for cliente in clientes:
+        # if cliente != nombre_cliente:
+        if cliente['nombre'] != nombre_cliente:
+            continue
+        else:
+            return True
+
+
+def _get_client_field(field_name, mensaje='Cual es la/el {} del cliente?'):
     field = None
     while not field:
-        field = input('Cual es la/el {} del cliente?'.format(field_name))
+        field = input(mensaje.format(field_name)).title().strip()
     return field
 
 
-def _get_nombre_cliente():
+def _get_client_email():
+    email_cliente = None
+    while not email_cliente:
+        email_cliente = input('Cual es el email del cliente?').strip()
+    return email_cliente
+
+
+def _get_client_from_user():
+    cliente = {
+        'nombre': _get_client_field('nombre'),
+        'empresa': _get_client_field('empresa'),
+        'email': _get_client_email(),
+        'puesto': _get_client_field('puesto'),
+    }
+
+    return cliente
+
+
+"""def _get_nombre_cliente():
     nombre_cliente = None
     while not nombre_cliente:
         nombre_cliente = input(
@@ -122,7 +135,19 @@ def _get_nombre_cliente():
     if not nombre_cliente:
         sys.exit()
 
-    return nombre_cliente
+    return nombre_cliente"""
+
+
+def _print_welcome():
+    # print('BIENVENIDO A' + FRED + ' RUGGERI VENTAS' + FR)
+    # print('*' * 50)
+    # print('Que quieres hacer hoy?')
+    print(FYE + '[C]rear Cliente' + FR)
+    print(FYE + '[L]ista Clientes' + FR)
+    print(FYE + '[E]liminar Cliente' + FR)
+    print(FYE + '[A]ctualizara Cliente' + FR)
+    print(FYE + '[B]uscar' + FR)
+    print(FYE + '[S]alir' + FR)
 
 
 if __name__ == '__main__':
@@ -136,27 +161,27 @@ while (True):
     command = input().upper()
 
     if command == 'C':
-        cliente = {
-            'nombre': _get_client_field('nombre'),
-            'empresa': _get_client_field('empresa'),
-            'email': _get_client_field('email'),
-            'puesto': _get_client_field('puesto'),
-        }
+        cliente = _get_client_from_user()
         crear_cliente(cliente)
         lista_clientes()
     elif command == 'L':
         lista_clientes()
     elif command == 'E':
-        nombre_cliente = _get_nombre_cliente()
-        borrar_cliente(nombre_cliente)
+        # nombre_cliente = _get_nombre_cliente()
+        cliente_id = int(_get_client_field('id'))
+        borrar_cliente(cliente_id)  # borrar_cliente(nombre_cliente)
         lista_clientes()
     elif command == 'A':
-        nombre_cliente = _get_nombre_cliente()
-        nombre_cliente_actualizado = input('Cual es el nuevo nombre?').title()
-        actualizar_cliente(nombre_cliente, nombre_cliente_actualizado)
+        # nombre_cliente = _get_nombre_cliente()
+        cliente_id = int(_get_client_field('id'))
+        # nombre_cliente_actualizado = input('Cual es el nuevo nombre?').title()
+        cliente_actualizado = _get_client_from_user()
+        # actualizar_cliente(nombre_cliente, nombre_cliente_actualizado)
+        actualizar_cliente(cliente_id, cliente_actualizado)
         lista_clientes()
     elif command == 'B':
-        nombre_cliente = _get_nombre_cliente()
+        # nombre_cliente = _get_nombre_cliente()
+        nombre_cliente = _get_client_field('nombre')
         found = buscar_cliente(nombre_cliente)
 
         if found:
